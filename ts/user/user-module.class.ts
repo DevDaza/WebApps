a7c3677fd1eaed5dcfd59bb7474379284be8a1e8
DevDaza <danielhassert@gmail.com>
2023-04-12 15:58:50 +0200

    added user rating

--- ts/user/user-module.class.ts
+++ ts/user/user-module.class.ts
@@ -1,4 +1,5 @@
 import { UserManagement } from "./user-management";
+import { UserRating } from "./user-rating.class";
 import { UserRole } from "./user-roles";
 import { User } from "./user.class";
 
@@ -9,15 +10,16 @@ export class UserModule {
     constructor() { }
 
     public init() {
-        const admin = new User({ name: 'Daza', surname: 'Development', age: 33 })
-        const adminRole = new UserRole('admin')
+        const admin = new User({ name: 'Daza', surname: 'Development', age: 33 });
+        const adminRole = new UserRole('admin');
+        const rating = new UserRating({ description: 'very-good', star: 9.5 });
 
         adminRole.setPermission('all')
         admin.setRole(adminRole)
+        admin.setRating(rating)
 
         this.userManagement = new UserManagement()
         this.userManagement.addUser(admin)
-
     }
 
 
--- ts/user/user.class.ts
+++ ts/user/user.class.ts
@@ -1,10 +1,12 @@
 import { UserData } from "./user-data.type";
+import { UserRating } from "./user-rating.class";
 import { UserRole } from "./user-roles";
 
 export class User {
 
     private role: UserRole = null;
     private previousRole: UserRole = null;
+    private rating: UserRating = null;
 
     constructor( public data: UserData ) {}
 
@@ -13,6 +15,13 @@ export class User {
         this.role = role;
     }
 
+    setRating(rating: UserRating) {
+        this.rating = rating;
+    }
+
+    getRating() {
+        return this.rating;
+    }
 
 
 
