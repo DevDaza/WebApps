5bcb93848c23bc9b85df9489c878fb688ec61df9
DevDaza <danielhassert@gmail.com>
2023-04-12 14:29:30 +0200

    super admin klasse entfernt

--- ts/user/user-module.class.ts
+++ ts/user/user-module.class.ts
@@ -1,28 +1,22 @@
 import { UserManagement } from "./user-management";
 import { UserRole } from "./user-roles";
 import { User } from "./user.class";
-import { SuperAdmin } from "./user.super-admin.class";
 
 export class UserModule {
 
     userManagement: UserManagement = null;
 
-    constructor() {}
+    constructor() { }
 
     public init() {
-
-        
         const admin = new User({ name: 'Daza', surname: 'Development', age: 33 })
         const adminRole = new UserRole('admin')
 
         adminRole.setPermission('all')
         admin.setRole(adminRole)
 
-        const superAdmin = new SuperAdmin({ name: 'Daza', surname: 'Development', age: 33 })
-        
         this.userManagement = new UserManagement()
         this.userManagement.addUser(admin)
-        this.userManagement.addUser(superAdmin)
 
     }
 
